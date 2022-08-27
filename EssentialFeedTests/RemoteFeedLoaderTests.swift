e481375299c06dc98b9c4dedfab942637025e4f1
Chad Games <chadgames@googlemail.com>
2022-08-27 08:46:19 -0700

    End of lecture "Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths"

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -20,7 +20,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "https://theurl.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
+        sut.load() { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
     }
@@ -29,10 +29,37 @@ class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "https://theurl.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
-        sut.load()
+        sut.load() { _ in }
+        sut.load() { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        
+        
+        var capturedError = [RemoteFeedLoader.Error]()
+        sut.load { capturedError.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.complete(with: clientError, at: 0)
+        
+        XCTAssertEqual(capturedError, [.connectivity])
+    }
+    
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        
+        let samples = [199, 201, 300, 400, 500].enumerated()
+        
+        samples.forEach({ index, sample in
+            var capturedError = [RemoteFeedLoader.Error]()
+            sut.load { capturedError.append($0) }
+            
+            client.complete(withStatusCode: sample, at: index)
+            XCTAssertEqual(capturedError, [.invalidData])
+        })
     }
     
     // MARK: - Helpers
@@ -46,11 +73,29 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     class HTTPClientSpy: HTTPClient {
         
-        var requestedURLs = [URL]()
+        private var messages = [(url: URL, completion: (HTTPResponseType) -> Void)]()
         
-        func get(from url: URL) {
-            requestedURLs.append(url)
+        var requestedURLs: [URL] {
+            return messages.map({ $0.url })
         }
+        
+        func get(from url: URL, completion: @escaping (HTTPResponseType) -> Void) {
+            messages.append((url, completion))
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURLs[index],
+                                           statusCode: code,
+                                           httpVersion: nil,
+                                           headerFields: nil)!
+            
+            messages[index].completion(.success(response))
+        }
+        
     }
     
 }
